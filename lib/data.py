bab4776c4f3f7328795eb2687f6bbaeafbcf20a1
clarenceluo78 <haoyanluo78@163.com>
2022-11-12 15:18:59 +0000

    update batch norm

--- lib/data.py
+++ lib/data.py
@@ -12,48 +12,6 @@ from matplotlib import pyplot as plt
 
 from . import utils
 
-###############################################
-# OO design (under construction)
-class DataModule(utils.HyperParameters):
-    """Defined in :numref:`subsec_oo-design-models`"""
-    def __init__(self, root='../data', num_workers=4):
-        self.save_hyperparameters()
-
-    def get_dataloader(self, train):
-        raise NotImplementedError
-
-    def train_dataloader(self):
-        return self.get_dataloader(train=True)
-
-    def val_dataloader(self):
-        return self.get_dataloader(train=False)
-
-    def get_tensorloader(self, tensors, train, indices=slice(0, None)):
-        """Use the more efficient API from PyTorch"""
-        tensors = tuple(a[indices] for a in tensors)
-        dataset = torch.utils.data.TensorDataset(*tensors)
-        return torch.utils.data.DataLoader(dataset, self.batch_size,
-                                           shuffle=train)
-
-class SyntheticRegressionData(DataModule):
-    """generates 1000 examples with 2-dimensional features drawn from a standard normal distribution
-    """
-    def __init__(self, w, b, noise=0.01, num_train=1000, num_val=1000,
-                 batch_size=32):
-        super().__init__()
-        self.save_hyperparameters()
-        n = num_train + num_val
-        self.X = torch.randn(n, len(w))
-        noise = torch.randn(n, 1) * noise
-        self.y = torch.matmul(self.X, w.reshape((-1, 1))) + b + noise
-
-    def get_dataloader(self, train):
-        """Defined in :numref:`sec_synthetic-regression-data`"""
-        i = slice(0, self.num_train) if train else slice(self.num_train, None)
-        return self.get_tensorloader((self.X, self.y), train, i)
-#####################################################3
-
-
 def synthetic_regression_data(w, b, num_examples):
     """generate y=Xw+b with noise"""
     X = torch.normal(0, 1, (num_examples, len(w)))
--- lib/utils.py
+++ lib/utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import numpy as np
 import torch
 import torchvision
@@ -20,160 +21,6 @@ def add_to_class(Class):
         setattr(Class, obj.__name__, obj)
     return wrapper
 
-############# This is base class that envolve hyperparameters #############
-class HyperParameters:
-    def save_hyperparameters(self, ignore=[]):
-        """Save function arguments into class attributes.
-        You can also ignore arguments."""
-        frame = inspect.currentframe().f_back
-        _, _, _, local_vars = inspect.getargvalues(frame)
-        self.hparams = {k:v for k, v in local_vars.items()
-                        if k not in set(ignore+['self']) and not k.startswith('_')}
-        for k, v in self.hparams.items():
-            setattr(self, k, v)
-
-############# This is base class for animated plotting #############
-class ProgressBoard(HyperParameters):
-    """Plot data points in animation.
-    ref: https://github.com/d2l-ai/d2l-en/blob/master/d2l/torch.py"""
-    def __init__(self, xlabel=None, ylabel=None, xlim=None,
-                 ylim=None, xscale='linear', yscale='linear',
-                 ls=['-', '--', '-.', ':'], colors=['C0', 'C1', 'C2', 'C3'],
-                 fig=None, axes=None, figsize=(3.5, 2.5), display=True):
-        self.save_hyperparameters()
-
-    def draw(self, x, y, label, every_n=1):
-        """Defined in :numref:`sec_utils`"""
-        Point = collections.namedtuple('Point', ['x', 'y'])
-        if not hasattr(self, 'raw_points'):
-            self.raw_points = collections.OrderedDict()
-            self.data = collections.OrderedDict()
-        if label not in self.raw_points:
-            self.raw_points[label] = []
-            self.data[label] = []
-        points = self.raw_points[label]
-        line = self.data[label]
-        points.append(Point(x, y))
-        if len(points) != every_n:
-            return
-        mean = lambda x: sum(x) / len(x)
-        line.append(Point(mean([p.x for p in points]),
-                          mean([p.y for p in points])))
-        points.clear()
-        if not self.display:
-            return
-        use_svg_display()
-        if self.fig is None:
-            self.fig = plt.figure(figsize=self.figsize)
-        plt_lines, labels = [], []
-        for (k, v), ls, color in zip(self.data.items(), self.ls, self.colors):
-            plt_lines.append(plt.plot([p.x for p in v], [p.y for p in v],
-                                          linestyle=ls, color=color)[0])
-            labels.append(k)
-        axes = self.axes if self.axes else plt.gca()
-        if self.xlim: axes.set_xlim(self.xlim)
-        if self.ylim: axes.set_ylim(self.ylim)
-        if not self.xlabel: self.xlabel = self.x
-        axes.set_xlabel(self.xlabel)
-        axes.set_ylabel(self.ylabel)
-        axes.set_xscale(self.xscale)
-        axes.set_yscale(self.yscale)
-        axes.legend(plt_lines, labels)
-        display.display(self.fig)
-        display.clear_output(wait=True)
-
-############# This is base module for neural network #############
-class Module(nn.Module, HyperParameters):
-    def __init__(self, plot_train_per_epoch=2, plot_valid_per_epoch=1):
-        super().__init__()
-        self.save_hyperparameters()
-        self.board = ProgressBoard() ##
-
-    def loss(self, y_hat, y):
-        raise NotImplementedError
-
-    def forward(self, X):
-        assert hasattr(self, 'net'), 'Neural network is defined'
-        return self.net(X)
-
-    def plot(self, key, value, train):
-        """Plot a point in animation."""
-        assert hasattr(self, 'trainer'), 'Trainer is not inited'
-        self.board.xlabel = 'epoch'
-        if train:
-            x = self.trainer.train_batch_idx / \
-                self.trainer.num_train_batches
-            n = self.trainer.num_train_batches / \
-                self.plot_train_per_epoch
-        else:
-            x = self.trainer.epoch + 1
-            n = self.trainer.num_val_batches / \
-                self.plot_valid_per_epoch
-        self.board.draw(x, numpy(to(value, torch.cpu())),
-                        ('train_' if train else 'val_') + key,
-                        every_n=int(n))
-
-    def training_step(self, batch):
-        l = self.loss(self(*batch[:-1]), batch[-1])
-        self.plot('loss', l, train=True)
-        return l
-
-    def validation_step(self, batch):
-        l = self.loss(self(*batch[:-1]), batch[-1])
-        self.plot('loss', l, train=False)
-
-    def configure_optimizers(self):
-        raise NotImplementedError
-
-############# This is base class for network training #############
-class Trainer(HyperParameters):
-    def __init__(self, max_epochs, num_gpus=0, gradient_clip_val=0):
-        self.save_hyperparameters()
-        assert num_gpus == 0, 'No GPU support yet'
-
-    def prepare_data(self, data):
-        self.train_dataloader = data.train_dataloader()
-        self.val_dataloader = data.val_dataloader()
-        self.num_train_batches = len(self.train_dataloader)
-        self.num_val_batches = (len(self.val_dataloader)
-                                if self.val_dataloader is not None else 0)
-
-    def prepare_model(self, model):
-        model.trainer = self
-        model.board.xlim = [0, self.max_epochs]
-        self.model = model
-
-    def fit(self, model, data):
-        self.prepare_data(data)
-        self.prepare_model(model)
-        self.optim = model.configure_optimizers()
-        self.epoch = 0
-        self.train_batch_idx = 0
-        self.val_batch_idx = 0
-        for self.epoch in range(self.max_epochs):
-            self.fit_epoch()
-
-    def fit_epoch(self):
-        raise NotImplementedError
-
-class SGD(HyperParameters):
-    def __init__(self, params, lr):
-        """Minibatch stochastic gradient descent."""
-        self.save_hyperparameters()
-
-    def step(self):
-        for param in self.params:
-            param -= self.lr * param.grad
-
-    def zero_grad(self):
-        for param in self.params:
-            if param.grad is not None:
-                param.grad.zero_()
-
-numpy = lambda x, *args, **kwargs: x.detach().numpy(*args, **kwargs)
-to = lambda x, *args, **kwargs: x.to(*args, **kwargs)
-#########################################################
-
 class Accumulator:
     """accumulate on n variables, can be used to calculate metrics"""
     def __init__(self, n):
@@ -188,3 +35,27 @@ class Accumulator:
     def __getitem__(self, idx):
         return self.data[idx]
 
+class Timer:
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        self.tik = time.time()
+
+    def stop(self):
+        """stop and record time"""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """return average time"""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """return total time"""
+        return sum(self.times)
+
+    def cumsum(self):
+        """return cumulative time"""
+        return np.array(self.times).cumsum().tolist()
