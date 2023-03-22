693cb2a5c2a6aeb627a16c6e4de724cfd4067b9c
clarenceluo78 <haoyanluo78@163.com>
2023-03-22 20:33:17 +0800

    refactor

--- metrics/silhouette_coef_from_scratch.py
+++ metrics/silhouette_coef_from_scratch.py
@@ -58,28 +58,5 @@ def silhouette_coef(X, y_pred):
 
     return np.mean(s_arr)
 
-def rand_index(y, y_pred):
-    """Rand index
-
-    Args:
-        y (_type_): true labels
-        y_pred (_type_): predict labels
-
-    Returns:
-        _type_: value of rand index
-    """
-    tp_plus_fp = comb(np.bincount(y), 2).sum()
-    tp_plus_fn = comb(np.bincount(y_pred), 2).sum()
-
-    A = np.c_[(y, y_pred)]
-
-    # calculate each component
-    tp = sum(comb(np.bincount(A[A[:, 0] == i, 1]), 2).sum()
-             for i in set(y))
-    fp = tp_plus_fp - tp
-    fn = tp_plus_fn - tp
-    tn = comb(len(A), 2) - tp - fp - fn
-
-    return (tp + tn) / (tp + fp + fn + tn)
 
 
